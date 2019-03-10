6e7fc8d1ca405a4b5581309bf2af4f194ef151be
Edward Brennan <EdMan1022@gmail.com>
2019-03-10 17:47:48 -0400

    Implement functions

--- exercises/multiples_3_5/utils.py
+++ exercises/multiples_3_5/utils.py
@@ -4,8 +4,10 @@ def divisible_by(value, divisors):
     """
     Raises a value error if the value isn't divisible by the divisors
     """
-    pass
-
+    for divisor in divisors:
+            if value % divisor == 0:
+                    return
+    raise ValueError
 
 def sum_of_values_divisible(max, divisors):
         """
@@ -14,4 +16,13 @@ def sum_of_values_divisible(max, divisors):
         As long as the value is divisible by at least one divisor,
         it is counted
         """
-        pass
+
+        divisible_sum = 0
+
+        for i in range(max):
+                try:
+                    divisible_by(i, divisors)
+                    divisible_sum += i
+                except ValueError:
+                    continue
+        return divisible_sum
