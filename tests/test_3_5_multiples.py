96ea00232b3857172e3cc1aa58c76d7852be9681
Edward Brennan <EdMan1022@gmail.com>
2019-03-10 15:05:11 -0400

    Add test skeleton

---
+++ tests/test_3_5_multiples.py
@@ -0,0 +1,14 @@
+from unittest import TestCase, main
+
+
+class TestMultiples(TestCase):
+
+    def test_divisible_by(self):
+
+
+
+        pass
+
+    def test_sum_of_divisors(self):
+        pass
+
