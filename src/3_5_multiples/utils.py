a7020025f814c941d9ee4a70b3462c0ae837a0e2
Edward Brennan <EdMan1022@gmail.com>
2019-03-10 15:07:33 -0400

    Add skeleton for solution

---
+++ src/3_5_multiples/utils.py
@@ -0,0 +1,16 @@
+
+
+def divisible_by(value, divisors):
+    """
+    Raises a value error if the value isn't divisible by the divisors
+    """
+    pass
+
+def sum_of_values_divisible(max, divisors):
+        """
+        Returns the sum of all values under max divisible by divisors
+
+        As long as the value is divisible by at least one divisor,
+        it is counted
+        """
+        pass
