24343942b27a99e5b550eead3309cdf4924796a1
Edward Brennan <EdMan1022@gmail.com>
2019-03-10 15:26:39 -0400

    Revert "Revert "Add code to test divisible_by function""
    This reverts commit 1b15edaa35e69b448d4ab7cad6a4cd1aee4cb241.

--- tests/test_3_5_multiples.py
+++ tests/test_3_5_multiples.py
@@ -1,9 +1,14 @@
 from unittest import TestCase, main
 
+from exercises.multiples_3_5 import divisible_by, sum_of_values_divisible
 
 class TestMultiples(TestCase):
 
     def test_divisible_by(self):
+
+        divisors = [2, 5]
+
+        divisible_by()
 
 
 
